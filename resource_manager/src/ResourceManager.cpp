7f7aff6cd5412a7efdf49252a7f70f4f2f050d92
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2020-09-24 17:07:39 -0700

    pal: avoid ecref setting for proxy type
    Change-Id: I2fffd03f3f7a8b5f6931eb67630b19d750c20eb7

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -1717,6 +1717,7 @@ int ResourceManager::registerDevice(std::shared_ptr<Device> d, Stream *s)
             }
         }
     } else if (sAttr.direction == QAL_AUDIO_OUTPUT &&
+        sAttr.type != QAL_STREAM_PROXY &&
         sAttr.type != QAL_STREAM_ULTRA_LOW_LATENCY) {
         status = s->getAssociatedDevices(associatedDevices);
         if (0 != status) {
