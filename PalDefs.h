3c3a92df6531ff87d17f8a1240851be39733f61b
Bala Murali Krishna Manchala <quic_bmanchal@quicinc.com>
2022-12-07 00:20:08 +0530

    pal: Fix copyrights in PalDefs.h
    Change-Id: Ica23aa23a4e383bfd384e14d74b0194a96b11de1
    Signed-off-by: Bala Murali Krishna Manchala <quic_bmanchal@quicinc.com>

--- PalDefs.h
+++ PalDefs.h
@@ -1,7 +1,3 @@
-<!--  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.--
->
-<!--  SPDX-License-Identifier: BSD-3-Clause-Clear                             --
->
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
@@ -29,6 +25,11 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 /** \file pal_defs.h
