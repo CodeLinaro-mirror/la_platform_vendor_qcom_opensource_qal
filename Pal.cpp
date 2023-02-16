tangalbert919/android_vendor_qcom_opensource_pal

pal: Add support for custom configurations of the backend
Add support for custom configurations of the backend.

Change-Id: Iff8932db7c7108e408c25364ab0deb170be97881

Pal.cpp
@@ -599,7 +599,14 @@ int32_t pal_stream_set_device(pal_stream_handle_t *stream_handle,
             devices, no_of_devices * sizeof(struct pal_device));
 
     for (int i = 0; i < no_of_devices; i++) {
-        rm->getDeviceInfo(pDevices[i].id, sattr.type, &devinfo);
+        if (strlen(pDevices[i].custom_config.custom_key)) {
+             PAL_DBG(LOG_TAG, "Device has custom key %s",
+                               pDevices[i].custom_config.custom_key);
+             rm->getDeviceInfo(pDevices[i].id, sattr.type,
+                               pDevices[i].custom_config.custom_key, &devinfo);
+        } else {
+             rm->getDeviceInfo(pDevices[i].id, sattr.type, &devinfo);
+        }
         if (devinfo.channels == 0 || devinfo.channels > devinfo.max_channels) {
             PAL_ERR(LOG_TAG, "Num channels[%d] is invalid", devinfo.channels);
             status = -EINVAL;

CodeLinaro-mirror/la_platform_vendor_qcom_opensource_qal

Pal.cpp
@@ -25,6 +25,12 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following lice
+nse:
+ *
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "PAL: API"
@@ -587,7 +593,14 @@ int32_t pal_stream_set_device(pal_stream_handle_t *stream_handle,
     }
 
     for (int i = 0; i < no_of_devices; i++) {
-        rm->getDeviceInfo(devices[i].id, sattr.type, &devinfo);
+        if (strlen(devices[i].custom_config.custom_key)) {
+             PAL_DBG(LOG_TAG, "Device has custom key %s",
+                               devices[i].custom_config.custom_key);
+             rm->getDeviceInfo(devices[i].id, sattr.type,
+                               devices[i].custom_config.custom_key, &devinfo);
+        } else {
+             rm->getDeviceInfo(devices[i].id, sattr.type, &devinfo);
+        }
         if (devinfo.channels == 0 || devinfo.channels > devinfo.max_channels) {
             PAL_ERR(LOG_TAG, "Num channels[%d] is invalid", devinfo.channels);
             return -EINVAL;