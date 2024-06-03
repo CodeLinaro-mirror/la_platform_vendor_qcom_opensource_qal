ba3e5d74f7f939beefa14c0e3aedb07f013fb4c8
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2024-06-03 18:16:54 +0530

    pal: Fix memory leak by free rd_buf after use
    During SSR, free rd_buf after reading the state
    
    Change-Id: I596dc71b6c3c842cb0d147d63f0baa1c624d9c21
    (cherry picked from commit dd5db83c1884e37e538c2b89a34ad33a97ee0cd5)

--- resource_manager/src/SndCardMonitor.cpp
+++ resource_manager/src/SndCardMonitor.cpp
@@ -29,7 +29,7 @@
  * Changes from Qualcomm Innovation Center are provided under the following
  * license:
  *
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -95,11 +95,13 @@ int  SndCardMonitor::onSndcardStateUpdate(sndcard_t *s)
     else {
         ret = -EINVAL;
         PAL_ERR(LOG_TAG, "unknown state, ret %d", ret);
+        free(rd_buf);
         return ret;
     }
 
     s->status = status;
     PAL_ERR(LOG_TAG, "state %d", status);
+    free(rd_buf);
 
     return status;
 }
