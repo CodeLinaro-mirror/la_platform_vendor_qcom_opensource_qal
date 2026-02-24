a706cdff73e76c34d978a067476b748a03462f4b
Kishan Kumar Majeti <kkmajeti@qti.qualcomm.com>
2026-02-24 10:18:39 +0530

    pal: Add correct markings for the file
    Add correct markings for Incall stream file
    
    Change-Id: Ie540f68d0e33ecf13050f5f263696ba67b10d9b5
    Signed-off-by: Kishan Kumar Majeti <kkmajeti@qti.qualcomm.com>

--- stream/src/StreamInCall.cpp
+++ stream/src/StreamInCall.cpp
@@ -1,11 +1,6 @@
 /*
  * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  *
- * Changes from Qualcomm Technologies, Inc. are provided under the following license:
- *
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
@@ -30,6 +25,12 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
  */
 
 #define LOG_TAG "StreamInCall"
